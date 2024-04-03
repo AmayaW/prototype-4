8169f7729f8850ff1adec46092ba6c91348c808d
Joyce Jiang <jj3115@barnard.edu>
2024-04-02 23:00:25 -0400

    modified player life script so player dies when colliding with enemy, and enemy dies when player collides with enemy from top

--- Assets/scripts/Player_Life.cs
+++ Assets/scripts/Player_Life.cs
@@ -8,18 +8,42 @@ public class Player_Life : MonoBehaviour
     private Rigidbody2D rb;
     private Animator anim;
 
+    //checking vertical offset threshold for top collision jump = player kills enemy
+    public float topCollisionThreshold = 0.5f;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();       
+        anim = GetComponent<Animator>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.CompareTag("Trap"))
+        Debug.Log("Collided with: " + collision.gameObject.tag);
+
+        if (collision.gameObject.CompareTag("Trap"))
         {
             Die();
+        }
+        else if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Vector3 hit = collision.contacts[0].normal;
+            float hitAngle = Vector3.Angle(hit, Vector3.up);
+            float verticalDifference = transform.position.y - collision.transform.position.y;
+
+            // Log collision details to understand the nature of the collision
+            Debug.Log($"Collision normal: {hit}, Angle: {hitAngle}, Vertical Difference: {verticalDifference}");
+
+            if (Mathf.Abs(hitAngle) < 90 && verticalDifference > topCollisionThreshold)
+            {
+                Debug.Log("Player killed the enemy by jumping on top.");
+                Destroy(collision.gameObject);
+            }
+            else
+            {
+                Debug.Log("Player died due to side collision with enemy.");
+                Die();
+            }
         }
     }
 
